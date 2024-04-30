c029f38b4086d64693bab10a24b08ae934f8633f
Sebastian Feistl <dev@bastion.codes>
2024-04-30 23:43:28 +0200

    Add reflection to resolve class name in LogUtility

--- Runtime/Logging/LogUtility.cs
+++ Runtime/Logging/LogUtility.cs
@@ -1,9 +1,43 @@
+using System;
+using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 
 namespace Bastion.Logging
 {
     public static class LogUtility
     {
+        public static string AddClassPrefix(string message, string color)
+        {
+            StackTrace stackTrace = new StackTrace();
+
+            // Get the frame representing the calling method
+            StackFrame frame = stackTrace.GetFrame(2);
+
+            if (frame != null)
+            {
+                // Get the method info of the calling method
+                MethodBase method = frame.GetMethod();
+
+                if (method != null)
+                {
+                    // Get the declaring type of the calling method
+                    Type declaringType = method.DeclaringType;
+
+                    if (declaringType != null)
+                    {
+                        // Construct the prefix using the class type
+                        string classPrefix = Colorize($"[{declaringType.Name}]", color) + " ";
+
+                        // Add the class prefix to the message
+                        message = classPrefix + message;
+                    }
+                }
+            }
+
+            return message;
+        }
+        
         public static string Colorize(string message, string hexColor)
         {
             return $"<color={hexColor}>{message}</color>";
