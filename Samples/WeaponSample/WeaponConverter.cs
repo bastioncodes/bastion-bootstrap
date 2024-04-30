d629b30b730c2c4246249c797a40bf9ffa611bb9
Sebastian Feistl <dev@bastion.codes>
2024-04-30 23:49:02 +0200

    Add WeaponConverter sample for advanced serialization customization

---
+++ Samples/WeaponSample/WeaponConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Reflex.Attributes;
+
+namespace Bastion.Samples
+{
+    // This is very useful for adding the ability to convert custom complex JSON structures automatically
+    // Read and Write is defined here, then the JsonConverter needs to be added to the configuration
+    // The config is currently set at the NewtonsoftJsonConverter constructor
+    public class WeaponConverter : JsonConverter
+    {
+        [Inject] private readonly WeaponFactory _factory;
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            WeaponData weapon = (WeaponData)value;
+            JObject obj = new JObject
+            {
+                { "name", weapon.Name },
+                { "damage", weapon.Damage },
+                { "cooldown", weapon.Cooldown },
+            };
+            obj.WriteTo(writer);
+        }
+
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+            JsonSerializer serializer)
+        {
+            JObject obj = JObject.Load(reader);
+            string name = (string)obj["name"];
+            int damage = (int)obj["damage"];
+            float cooldown = (float)obj["cooldown"];
+
+            return _factory.Create(name, damage, cooldown);
+        }
+
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(WeaponData);
+        }
+    }
+}
