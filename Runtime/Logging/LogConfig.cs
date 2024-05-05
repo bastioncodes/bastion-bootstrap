bb5497444c2c073c9cc246493c0a6e4060e07a69
Sebastian Feistl <dev@bastion.codes>
2024-05-05 12:57:22 +0200

    Add LogConfig and LoggableAttribute as replacement to the ILoggable interface

---
+++ Runtime/Logging/LogConfig.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Bastion.Logging
+{
+    public static class LogConfig
+    {
+        public static Dictionary<Type, LoggableInfo> LoggableTypes { get; } = new ();
+
+        public static void Initialize()
+        {
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (var type in assembly.GetTypes())
+                {
+                    // Find all types that have the LoggableAttribute
+                    var loggableAttribute = type.GetCustomAttribute<LoggableAttribute>();
+                    if (loggableAttribute == null) continue;
+                    
+                    BastionLogger.LogInfo($"Loggable Attribute found ({type.Name}), caching now.");
+                        
+                    LoggableTypes[type] = new LoggableInfo
+                    {
+                        EnableLogging = loggableAttribute.Enable,
+                        LogName = loggableAttribute.Name,
+                        LogColor = loggableAttribute.Color,
+                        LogChannel = loggableAttribute.Channel
+                    };
+                }
+                
+                /*
+                foreach (var type in assembly.GetTypes())
+                {
+                    // Check if the type implements ILoggable
+                    if (!typeof(ILoggable).IsAssignableFrom(type) || type.IsInterface || type.IsAbstract) continue;
+                    if (Activator.CreateInstance(type) is not ILoggable instance) continue;
+                    
+                    // Cache the ILoggable data so it only needs to be reflected once
+                    LoggableTypes[type] = new LoggableInfo
+                    {
+                        EnableLogging = instance.EnableLogging,
+                        LogName = instance.LogName,
+                        LogColor = instance.LogColor,
+                        LogChannel = instance.LogChannel
+                    };
+                        
+                    BastionLogger.LogWarning(instance.LogColor);
+                    BastionLogger.LogWarning(instance.LogName);
+                }
+                */
+            }
+            
+            BastionLogger.LogSuccess("DONE: " + LoggableTypes.Count + " items cached.");
+        }
+
+        public class LoggableInfo
+        {
+            public bool EnableLogging { get; set; }
+            public string LogName { get; set; }
+            public string LogColor { get; set; }
+            public string LogChannel { get; set; }
+        }
+    }
+
+}
---
+++ Runtime/Logging/LoggableAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bastion.Logging
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public sealed class LoggableAttribute : Attribute
+    {
+        public bool Enable { get; set; } = true;
+        public string Name { get; set; } = "";
+        public string Color { get; set; } = Theme.Color.Sky;
+        public string Channel { get; set; } = "Default";
+    }
+}
