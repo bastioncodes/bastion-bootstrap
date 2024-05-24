4bbb9aca2bb5ff2ad753b1e8a729330fc165386b
Sebastian Feistl <dev@bastion.codes>
2024-05-24 13:29:49 +0200

    Add theme color codes

--- Runtime/Theme/Color.cs
+++ Runtime/Theme/Color.cs
@@ -3,10 +3,16 @@ namespace Bastion.Theme
     public static class Color
     {
         public const string Red = "#ef4444";
-        public const string Yellow = "#f59e0b";
+        public const string Pink = "#db2777";
+        public const string Violet = "#8b5cf6";
+        public const string Amber = "#f59e0b";
+        public const string Yellow = "#facc15";
+        public const string Orange = "#f97316";
         public const string Blue = "#0ea5e9";
         public const string Sky = "#7dd3fc";
         public const string Green = "#22c55e";
+        public const string Teal = "#14b8a6";
+        public const string Lime = "#84cc16";
         public const string DarkGray = "#84858c";
         public const string LightGray = "#a6a6a6";
     }
