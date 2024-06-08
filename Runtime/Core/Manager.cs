aa560ae7d9dbf3d74361f12153d6907b23990dc0
Sebastian Feistl <dev@bastion.codes>
2024-06-08 22:40:21 +0200

    Move session counter to new UserSessionCounter and add static ManagerInitialized event to Manager base class

--- Runtime/Core/Manager.cs
+++ Runtime/Core/Manager.cs
@@ -8,12 +8,25 @@ namespace Bastion.Core
     /// </summary>
     public abstract class Manager : IDisposable
     {
+        public static event Action<Type> ManagerInitialized;
+        
         /// <summary>
         /// Initializes the manager, with optional completion and error handling callbacks.
         /// </summary>
         /// <param name="onComplete">Callback invoked when initialization is successfully completed.</param>
         /// <param name="onError">Callback invoked if an initialization error occurs.</param>
         public abstract void Initialize(Action onComplete = null, Action<Exception> onError = null);
+        
+        /// <summary>
+        /// Notifies subscribers about the completion of the manager's initialization.
+        /// This method should be called during the initialization to trigger global notifications.
+        /// </summary>
+        /// <param name="onComplete">The appropriate Action of the <see cref="Initialize"/> method.</param>
+        protected void NotifyCompleted(Action onComplete)
+        {
+            ManagerInitialized?.Invoke(GetType());
+            onComplete?.Invoke();
+        }
         
         /// <summary>
         /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
