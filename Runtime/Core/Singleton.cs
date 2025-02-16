3ece2a457d921dae3ef9ee9bb4b69238edf4cb00
Sebastian Feistl <dev@bastion.codes>
2025-02-16 11:33:37 +0100

    Add generic MonoBehaviour Singleton

---
+++ Runtime/Core/Singleton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Bastion.Core
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+        private static readonly object _lock = new();
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance != null) return _instance;
+                lock (_lock)
+                {
+                    _instance = FindFirstObjectByType<T>();
+
+                    if (_instance != null) return _instance;
+                    var singletonObject = new GameObject(typeof(T).Name);
+                    _instance = singletonObject.AddComponent<T>();
+                    DontDestroyOnLoad(singletonObject);
+                }
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = (T)(object)this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
